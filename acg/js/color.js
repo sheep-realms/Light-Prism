55115cb12cce67a06d43389031009653a646095a
Sheep-realms <38580457+sheep-realms@users.noreply.github.com>
2021-09-07 01:11:11 +0800

    Add files via upload

--- acg/js/color.js
+++ acg/js/color.js
@@ -1,8 +1,28 @@
 let colorHair = [
     {
+        name: "响 银",
+        color: "#F2F3F5",
+        group: "舰队Collection"
+    }, {
         name: "伊蕾娜 灰",
         color: "#EEF0F3",
         group: "魔女之旅"
+    }, {
+        name: "金刚光 银",
+        color: "#EBEBF3",
+        group: "干物妹小埋"
+    }, {
+        name: "拉菲 白",
+        color: "#EDEBFC",
+        group: "碧蓝航线"
+    }, {
+        name: "凌波 银",
+        color: "#FAF3EB",
+        group: "碧蓝航线"
+    }, {
+        name: "洛天依 灰",
+        color: "#8A8B9C",
+        group: "VOCALOID"
     }, {
         name: "田村日和 黑",
         color: "#6B6370",
@@ -16,13 +36,29 @@ let colorHair = [
         color: "#AF7B66",
         group: "某科学的超电磁炮"
     }, {
-        name: "日下部美纱绪 褐",
+        name: "电 棕",
+        color: "#96673D",
+        group: "舰队Collection"
+    }, {
+        name: "雷 棕",
+        color: "#9B5134",
+        group: "舰队Collection"
+    }, {
+        name: "日下部美纱绪 棕",
         color: "#7F675A",
         group: "幸运☆星"
+    }, {
+        name: "乐正绫 棕",
+        color: "#533D40",
+        group: "VOCALOID"
     }, {
         name: "高良美幸 粉",
         color: "#F8CFD7",
         group: "幸运☆星"
+    }, {
+        name: "巡音流歌 粉",
+        color: "#FAAFBE",
+        group: "VOCALOID"
     }, {
         name: "小神晶 粉",
         color: "#EF8D9A",
@@ -32,9 +68,17 @@ let colorHair = [
         color: "#EC7F86",
         group: "幸运☆星"
     }, {
+        name: "夏娜 红",
+        color: "#FA5768",
+        group: "灼眼的夏娜"
+    }, {
         name: "五河琴里 红",
         color: "#E56179",
         group: "约会大作战"
+    }, {
+        name: "镜音双子 金",
+        color: "#FFF380",
+        group: "VOCALOID"
     }, {
         name: "帕特莉西亚 金",
         color: "#FCD793",
@@ -59,6 +103,10 @@ let colorHair = [
         name: "岩崎南 绿",
         color: "#8CCCB0",
         group: "幸运☆星"
+    }, {
+        name: "初音未来 绿",
+        color: "#39C5BB",
+        group: "VOCALOID"
     }, {
         name: "明石 绿",
         color: "#92C3A1",
@@ -87,6 +135,10 @@ let colorHair = [
         name: "五河士道 蓝",
         color: "#626393",
         group: "约会大作战"
+    }, {
+        name: "晓 蓝",
+        color: "#4D435C",
+        group: "舰队Collection"
     }, {
         name: "柊镜 柊司 紫",
         color: "#C4AED4",
